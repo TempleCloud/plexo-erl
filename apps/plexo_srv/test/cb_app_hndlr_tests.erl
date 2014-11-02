188b475e6de9a0fe595994ed37b368c3de5ffc7c
TempleCloud <tim.langford@gmail.com>
2014-11-02 01:48:10 +0000

    Updated simple http based tests.

--- apps/plexo_srv/test/cb_app_hndlr_tests.erl
+++ apps/plexo_srv/test/cb_app_hndlr_tests.erl
@@ -19,7 +19,11 @@ cb_app_hndlr_test_() -> {
   {setup, local,
     fun start_plexo_srv/0, fun stop_plexo_srv/1, fun test_start_app/1},
   {setup, local,
+    fun start_plexo_srv/0, fun stop_plexo_srv/1, fun test_start_started_app/1},
+  {setup, local,
     fun start_plexo_srv/0, fun stop_plexo_srv/1, fun test_stop_app/1},
+  {setup, local,
+    fun start_plexo_srv/0, fun stop_plexo_srv/1, fun test_stop_stopped_app/1},
   {setup, local,
     fun start_plexo_srv/0, fun stop_plexo_srv/1, fun test_get_app/1}
   ]
@@ -32,10 +36,8 @@ cb_app_hndlr_test_() -> {
 start_plexo_srv() ->
   plexo_srv:start(),
   inets:start(),
-
-  RestPath = "http://localhost:8877/api/app",
-  App = "sasl",
-
+  RestPath = <<"http://localhost:8877/api/app">>,
+  App = <<"sasl">>,
   {RestPath, App}.
 
 stop_plexo_srv(_TestConfig) ->
@@ -48,18 +50,53 @@ stop_plexo_srv(_TestConfig) ->
 %%%============================================================================
 
 test_start_app({RestPath, App}) ->
+  Res = start_remote_app(RestPath, App),
+  #{<<"appName">> := AppName} = Res,
+  #{<<"appStatus">> := AppStatus} = Res,
+  ?_assertEqual(AppName, App),
+  ?_assertEqual(AppStatus, <<"app_started">>).
+
+test_start_started_app({RestPath, App}) ->
   start_remote_app(RestPath, App),
-  ?_assertEqual(ok, ok).
+  Res = start_remote_app(RestPath, App),
+  #{<<"appName">> := AppName} = Res,
+  #{<<"appStatus">> := AppStatus} = Res,
+  ?_assertEqual(AppName, App),
+  ?_assertEqual(AppStatus, <<"app_running">>).
 
 test_stop_app({RestPath, App}) ->
   start_remote_app(RestPath, App),
-  stop_remote_app(RestPath, App),
-  ?_assertEqual(ok, ok).
+  Res = stop_remote_app(RestPath, App),
+  #{<<"appName">> := AppName} = Res,
+  #{<<"appStatus">> := AppStatus} = Res,
+  ?_assertEqual(AppName, App),
+  ?_assertEqual(AppStatus, <<"app_stopped">>).
 
+test_stop_stopped_app({RestPath, App}) ->
+  Res = stop_remote_app(RestPath, App),
+  #{<<"appName">> := AppName} = Res,
+  #{<<"appStatus">> := AppStatus} = Res,
+  ?_assertEqual(AppName, App),
+  ?_assertEqual(AppStatus, <<"app_not_running">>).
 
 test_get_app({RestPath, App}) ->
   start_remote_app(RestPath, App),
-  get_remote_app(RestPath, App),
+  Res = get_remote_app(RestPath, App),
+  #{
+    <<"applications">> := _Applications,
+    <<"description">> := _Description,
+    <<"env">> := _Env,
+    <<"id">> := Id,
+    <<"included_applications">> := _IncludedApplications,
+    <<"maxP">> := _MaxP,
+    <<"maxT">> := _MaxT,
+    <<"mod">> := #{<<"name">> := ModName, <<"params">> := _ModParams},
+    <<"modules">> := _Modules,
+    <<"registered">> := _Registered,
+    <<"start_phases">> := _StartPhase,
+    <<"vsn">> := _VSN
+  } = Res,
+  ?_assertEqual(ModName, App),
   ?_assertEqual(ok, ok).
 
 
@@ -69,7 +106,7 @@ test_get_app({RestPath, App}) ->
 
 start_remote_app(RestPath, App) ->
 
-  Url = RestPath ++ "/" ++ App,
+  Url = binary_to_list(RestPath) ++ "/" ++ binary_to_list(App),
   ContentType = "application/x-www-form-urlencoded",
   RqBody = <<"">>,
 
@@ -83,7 +120,7 @@ start_remote_app(RestPath, App) ->
 
 stop_remote_app(RestPath, App) ->
 
-  Url = RestPath ++ "/" ++ App,
+  Url = binary_to_list(RestPath) ++ "/" ++ binary_to_list(App),
 
   {ok, {{_HttpVsn, 200, _StatusCode}, _RsHeaders, RsBody}} =
     httpc:request(delete, {Url, []}, [], []),
@@ -95,7 +132,7 @@ stop_remote_app(RestPath, App) ->
 
 get_remote_app(RestPath, App) ->
 
-  Url = RestPath ++ "/" ++ App,
+  Url = binary_to_list(RestPath) ++ "/" ++ binary_to_list(App),
 
   {ok, {{_HttpVsn, 200, _StatusCode}, _Headers, RsBody}} =
     httpc:request(get, {Url, []}, [], []),
--- apps/plexo_srv/test/cb_apps_hndlr_tests.erl
+++ apps/plexo_srv/test/cb_apps_hndlr_tests.erl
@@ -8,5 +8,96 @@
 -module(cb_apps_hndlr_tests).
 -author("Temple").
 
-%% API
--export([]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+%%%============================================================================
+%% Test Descriptions
+%%%============================================================================
+
+cb_app_hndlr_test_() -> {
+  "Test starting the applications required for the plexo_srv application.", [
+    {setup, local,
+      fun start_plexo_srv/0, fun stop_plexo_srv/1, fun test_start_app/1},
+    {setup, local,
+      fun start_plexo_srv/0, fun stop_plexo_srv/1, fun test_start_started_app/1},
+    {setup, local,
+      fun start_plexo_srv/0, fun stop_plexo_srv/1, fun test_get_running_apps/1},
+    {setup, local,
+      fun start_plexo_srv/0, fun stop_plexo_srv/1, fun test_get_loaded_apps/1}
+  ]
+}.
+
+%%%============================================================================
+%% Setup Functions
+%%%============================================================================
+
+start_plexo_srv() ->
+  plexo_srv:start(),
+  inets:start(),
+  RestPath = <<"http://localhost:8877/api/apps">>,
+  App = <<"sasl">>,
+  {RestPath, App}.
+
+stop_plexo_srv(_TestConfig) ->
+  plexo_srv:stop(),
+  inets:stop(),
+  ok.
+
+%%%============================================================================
+%% Tests
+%%%============================================================================
+
+test_start_app({RestPath, App}) ->
+  Res = start_remote_app(RestPath, App),
+  #{<<"appName">> := AppName} = Res,
+  #{<<"appStatus">> := AppStatus} = Res,
+  ?_assertEqual(AppName, App),
+  ?_assertEqual(AppStatus, <<"app_started">>).
+
+test_start_started_app({RestPath, App}) ->
+  start_remote_app(RestPath, App),
+  Res = start_remote_app(RestPath, App),
+  #{<<"appName">> := AppName} = Res,
+  #{<<"appStatus">> := AppStatus} = Res,
+  ?_assertEqual(AppName, App),
+  ?_assertEqual(AppStatus, <<"app_running">>).
+
+test_get_running_apps({RestPath, _App}) ->
+  Res = get_remote_apps(RestPath, <<"?status=running">>),
+  ?_assertEqual(Res, Res).
+
+test_get_loaded_apps({RestPath, _App}) ->
+  Res = get_remote_apps(RestPath, <<"?status=loaded">>),
+  ?_assertEqual(Res, Res).
+
+
+
+%%%============================================================================
+%% Helper Methods
+%%%============================================================================
+
+start_remote_app(RestPath, App) ->
+
+  Url = binary_to_list(RestPath) ++ "/" ++ binary_to_list(App),
+  ContentType = "application/x-www-form-urlencoded",
+  RqBody = <<"">>,
+
+  {ok, {{_HttpVsn, 200, _StatusCode}, _Headers, RsBody}} =
+    httpc:request(post, {Url, [], ContentType, RqBody}, [], []),
+
+  Res = core_json:from_json(RsBody),
+  ?debugFmt("Created App Resource: ~p~n", [Res]),
+  Res.
+
+
+get_remote_apps(RestPath, QueryParam) ->
+
+  Url = binary_to_list(RestPath) ++ binary_to_list(QueryParam),
+
+  {ok, {{_HttpVsn, 200, _StatusCode}, _Headers, RsBody}} =
+    httpc:request(get, {Url, []}, [], []),
+
+  Res = core_json:from_json(RsBody),
+  ?debugFmt("Retrieved App Resource: ~p~n", [Res]),
+  Res.
